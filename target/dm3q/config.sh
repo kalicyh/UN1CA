222625373fee1d6a99e76c65f1710d3980525630
kalicyh <34980061+kalicyh@users.noreply.github.com>
2026-02-01 22:55:27 +0800

    🧪 test: fork from fsrb-android-dev/UN1CA

---
+++ target/dm3q/config.sh
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy S23 Ultra (dm3q)
+TARGET_NAME="Galaxy S23 Ultra"
+TARGET_CODENAME="dm3q"
+TARGET_PLATFORM="sm8550"
+TARGET_FIRMWARE="SM-S918B/EUX/350196551234562"
+TARGET_EXTRA_FIRMWARES=()
+
+# SEC Product Feature
+TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL=true
+TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME="siop_dm3q_sm8550"
+TARGET_LCD_CONFIG_HFR_MODE="3"
+TARGET_WLAN_SUPPORT_MOBILEAP_DUALAP=false
